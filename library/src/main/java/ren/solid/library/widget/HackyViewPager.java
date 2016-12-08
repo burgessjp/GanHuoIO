fe6919912c3fdd7e2229c3ad36b40ef48394f3e2
burgessjp <1104160707@qq.com>
2016-12-08 10:44:51 +0800

    fix imageview zoom size crash exception

---
+++ library/src/main/java/ren/solid/library/widget/HackyViewPager.java
@@ -0,0 +1,34 @@
+package ren.solid.library.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by _SOLID
+ * Date:2016/12/8
+ * Time:10:38
+ * Desc:
+ */
+
+public class HackyViewPager extends ViewPager {
+    public HackyViewPager(Context context) {
+        super(context);
+    }
+
+    public HackyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            //uncomment if you really want to see these errors
+            //e.printStackTrace();
+            return false;
+        }
+    }
+}

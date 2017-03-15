786532f9ba3fb2945ac67a8e9d68419ee122f062
burgessjp <1104160707@qq.com>
2017-03-15 16:32:47 +0800

    delete library's unitTest content

--- library/src/test/java/ren/solid/library/ExampleUnitTest.java
+++ library/src/test/java/ren/solid/library/ExampleUnitTest.java
@@ -2,19 +2,11 @@ package ren.solid.library;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-
-        String baseUrl = "http://gank.io/api/";
-        String url = "http://gank.io/api/day/history/2016/07/26";
-
-
-        assertEquals("day/history", url.substring(baseUrl.length()));
     }
 }
